65d364bf6d54bd61eca23fa158ef91439238fb1d
Rakesh Raj <cgpa10.0@gmail.com>
2021-06-06 12:12:53 +0530

    Nav Bar of Amazon Clone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello Me!!</h1>
+      <Header></Header>
+      {/*Home*/}
     </div>
   );
 }
